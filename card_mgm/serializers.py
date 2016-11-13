886339ee3c6f8ac8f5fb2d7d038d615aeff188b8
Thomas Boros <tomas.boros92@gmail.com>
2016-11-13 22:37:45 +0100

    added gitignore and missing files from repo

---
+++ card_mgm/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from models import RFid
+from django.contrib.auth.models import User
+
+class RFidSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField()
+    class Meta:
+        model = RFid
+        fields = ('id', 'rfid', 'token_type', 'token_status', 'user')
+
+class RFidCreateSerializer(serializers.ModelSerializer):
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
+    class Meta:
+        model = RFid
+        fields = ('id', 'rfid', 'token_type', 'token_status', 'user')
