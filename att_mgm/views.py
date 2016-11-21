9eeccb5a0111d72fed5fc157896aeb734e8e597a
Thomas Boros <tomas.boros92@gmail.com>
2016-11-21 18:55:47 +0100

    Implemented prototype of access

--- att_mgm/views.py
+++ att_mgm/views.py
@@ -8,6 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 
 from django_filters.rest_framework.backends import DjangoFilterBackend
 
+from card_mgm.models import RFid
+
 
 from serializers import AttendanceEntrySerializer, CreateAttendanceEntrySerializer
 from models import AttendanceEntry
@@ -24,6 +26,21 @@ class AttendanceEntryViewSet(ModelViewSet):
         serializer = CreateAttendanceEntrySerializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
+
+            try:
+                rfid = RFid.objects.get(rfid=request.data['rfid'])
+            except RFid.DoesNotExist:
+                return Response({'Error': 'RFid token not found in DB'}, status=status.HTTP_404_NOT_FOUND)
+
+            user = rfid.user
+
+            device = request.user
+
+            #TODO add restriction so only devices may access this call
+            if user.has_perm('can_enter', device.location):
+                return Response(data=serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(data={'Error': 'Access denied'}, status=status.HTTP_403_FORBIDDEN)
 
             return Response(data=serializer.data, status=status.HTTP_200_OK)
 
